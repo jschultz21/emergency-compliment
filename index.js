6d992bfb51cf7a10f3fd899bb9ccc258d34efd76
Julia Schultz <julia.schultz91@gmail.com>
2016-07-24 11:13:38 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var app = express();
+
+app.listen(4000, function(){
+  console.log("app listening on port 4000");
+});
+
+
+app.get("/", function(req, res){
+  res.send("Hi there!");
+});
