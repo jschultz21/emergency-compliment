646f40f7ed7929e7e307d2c21f961e3dc3790b9a
Julia Schultz <julia.schultz91@gmail.com>
2016-07-24 14:33:56 -0400

    form for name working

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ var express = require("express");
 var app = express();
 var compliments = require("./compliment.js");
 var colors = require("./color.js");
+var bodyParser = require("body-parser");
 
-
+app.use(bodyParser.json()); //handles json post requests
+app.use(bodyParser.urlencoded({ extended: true })); // handles form submissions
 
 app.set("view engine", "hbs");
 app.use(express.static("public"));
@@ -19,6 +21,11 @@ app.get("/", function(req, res){
   res.render("index", {compliment: randomCompliment, color: randomColor});
 });
 
+app.get("/:name/new", function(req, res){
+  var randomColor = colors.getRandomColor();
+  res.render("new", {color: randomColor});
+});
+
 app.get("/:name", function(req, res){
   var randomCompliment = compliments.getRandomCompliment();
   var randomColor = colors.getRandomColor();
@@ -26,6 +33,23 @@ app.get("/:name", function(req, res){
 });
 
 
-app.get("/new", function(req, res){
-  res.render("new");
+// app.post("/:name/new", function(req, res){
+//   var randomColor = colors.getRandomColor();
+//
+//   res.render("index",{
+//     new_compliment:req.body.player_name, color:randomColor
+//   })
+// })
+
+app.post("/", function(req, res){
+  var name = req.body.player_name;
+  res.redirect('/' + name);
+});
+
+
+
+app.post("/:name/new", function(req, res){
+  compliments.push(req.body.new_compliment);
+  var name = req.params.name;
+  res.redirect('/' + name);
 });
